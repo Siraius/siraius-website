22c8ff8d95889c7230ba45eb662c8004d1597136
Kelton Chesshire <kclittlered12@gmail.com>
2022-07-06 19:49:35 -0500

    Adjusted Site URL

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -5,7 +5,7 @@ const siteMetadata = {
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
+  siteUrl: 'https://siraius.vercel.app',
   siteRepo: 'https://github.com/Siraius/siraius-website',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
